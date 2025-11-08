186ce5089e1ce57db589e60528b3c557352aaf54
Yingsan1987 <Yingsan1987@gmail.com>
2025-11-08 15:58:53 -0700

    Update Events.jsx

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -408,7 +408,7 @@ const Events = () => {
                       </div>
                     )}
                   </div>
-                  )}
+                )}
                 </div>
               );
               })}
